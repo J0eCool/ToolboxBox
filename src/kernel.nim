b10c8ad0fe87fdc901262281e0f45738f8d47e62
J0eCool <count.j0ecool@gmail.com>
2021-01-17 20:33:01 -0500

    keyboard + square

--- src/kernel.nim
+++ src/kernel.nim
@@ -4,10 +4,14 @@ proc main() =
     discard sdl2.init(INIT_EVERYTHING)
 
     let (winW, winH) = (1200, 900)
-    let window = createWindow("ToolboxBox", 100, 100, winW.cint, winH.cint, SDL_WINDOW_SHOWN)
+    let window = createWindow("ToolboxBox",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        winW.cint, winH.cint, SDL_WINDOW_SHOWN)
     defer:
         destroy window
-    let renderFlags = Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture
+
+    let renderFlags = (Renderer_Accelerated or Renderer_PresentVsync or
+        Renderer_TargetTexture)
     let render = createRenderer(window, -1, renderFlags)
     defer:
         destroy render
@@ -18,16 +22,30 @@ proc main() =
     while runGame:
         var evt = sdl2.defaultEvent
         while pollEvent(evt):
-            if evt.kind == QuitEvent:
+            case evt.kind
+            of QuitEvent:
                 runGame = false
                 break
+            of KeyDown:
+                case evt.key.keysym.sym
+                of K_ESCAPE:
+                    runGame = false
+                    break
+                else:
+                    discard
+            else:
+                discard
 
         inc t
-        t = t mod 256
 
-        render.setDrawColor t.uint8,0,0,255
+        render.setDrawColor t.uint8, 0, 0, 255
         render.clear
+
+        render.setDrawColor 0, 255, 255, 255
+        var r = rect(20, 20, 80, 80)
+        render.fillRect r
+
         render.present
 
-echo "Helo werl"
+echo "Helo wrerl"
 main()
