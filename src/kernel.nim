82e884c73e55caf788f58bdfb430f703c6880429
J0eCool <count.j0ecool@gmail.com>
2021-01-20 03:27:11 -0500

    First sketch of module import+export+context tracking (holy frick that worked)

--- src/kernel.nim
+++ src/kernel.nim
@@ -5,24 +5,34 @@ import sdl2
 import vec
 
 type
-    posFunc = proc(t: float): Vec {.cdecl.}
-    updateFunc = proc(t: float, callback: proc(pos: Vec) {.cdecl.}) {.cdecl.}
+    Context = ptr ContextObj
+    ContextObj = object
+        allocate: proc(t: Natural): pointer {.cdecl.}
+        drawBox: proc(ctx: Context, pos, size: Vec) {.cdecl.}
+        # private-ish var
+        render: RendererPtr
 
-let lib = loadLib("testlib.dll")
-assert lib != nil
-let getPos = cast[posFunc](lib.symAddr("positionForTime"))
-let wrapUpdate = cast[updateFunc](lib.symAddr("update"))
+    Library = ptr object
+        update: proc(lib: Library, t: float) {.cdecl.}
 
-var globalRenderer: RendererPtr
+proc allocate(size: Natural): pointer {.cdecl.} =
+    alloc(size)
 
 proc drawBox(render: RendererPtr, pos, size: Vec) =
     var rec = rect(pos.x.cint, pos.y.cint, size.x.cint, size.y.cint)
     render.fillRect rec
 
-proc drawWrapBox(pos: Vec) {.cdecl.} =
-    globalRenderer.drawBox(pos, vec(40, 40))
+proc drawWrapBox(ctx: Context, pos, size: Vec) {.cdecl.} =
+    ctx.render.drawBox(pos, size)
+
+proc initialize(render: RendererPtr): Context =
+    result = cast[Context](allocate(sizeof(ContextObj)))
+    result.allocate = allocate
+    result.drawBox = drawWrapBox
+    result.render = render
 
 proc main() =
+    # set up sdl and window and such
     discard sdl2.init(INIT_EVERYTHING)
 
     let (winW, winH) = (1200, 900)
@@ -35,10 +45,19 @@ proc main() =
     let renderFlags = (Renderer_Accelerated or Renderer_PresentVsync or
         Renderer_TargetTexture)
     let render = createRenderer(window, -1, renderFlags)
-    globalRenderer = render
     defer:
         destroy render
 
+    # set up self module
+    let ctx = initialize(render)
+
+    # do dll loady things
+    let libDll = loadLib("testlib.dll")
+    assert libDll != nil
+    let libInit = cast[proc(ctx: Context): Library {.cdecl.}](libDll.symAddr("initialize"))
+    let module = libInit(ctx)
+
+    # run loop, logic
     var runGame = true
     var t = 0.0
 
@@ -69,11 +88,7 @@ proc main() =
         render.drawBox(vec(20, 20), vec(80, 80))
 
         render.setDrawColor 128, 64, 255, 255
-        let pos = getPos(t)
-        render.drawBox(pos, vec(80, 80))
-
-        render.setDrawColor 0, 0, 0, 255
-        wrapUpdate(t, drawWrapBox)
+        module.update(module, t)
 
         render.present
 
--- src/testlib.nim
+++ src/testlib.nim
@@ -2,10 +2,39 @@ import math
 
 import vec
 
-{.pragma:expfunc, cdecl, exportc, dynlib.}
 
-proc positionForTime(t: float): Vec {.expfunc.} =
-    vec(120 + 10 * cos(5 * t), 200 + 100 * sin(t))
+# {.pragma:expfunc, cdecl, exportc, dynlib.}
+# {.pragma impfunc: cdecl.}
 
-proc update(t: float, draw: proc(pos: Vec) {.cdecl.}) {.expfunc.} =
-    draw(vec(300, 30))
+type
+    Renderer = ptr object
+    Imports = ptr object
+        allocate: proc(t: Natural): pointer {.cdecl.}
+        drawBox: proc(imports: Imports, pos, size: Vec) {.cdecl.}
+
+    Context = ptr ContextObj
+    ContextObj = object
+        update: proc(ctx: Context, t: float) {.cdecl.}
+        # private field comes after exports (yikes)
+        imports: Imports
+
+# forward declaration
+proc update(ctx: Context, t: float) {.cdecl.}
+
+proc initialize(imports: Imports): Context {.cdecl, exportc, dynlib.} =
+    # going to need to rethink actual import object as a lookup for procs,
+    # rather than a raw struct, because we don't want this to be sensitive to
+    # pointer layout, and also we want to be able to only use a subset of the
+    # passed-in library
+    result = cast[Context](imports.allocate(sizeof(ContextObj)))
+    result.imports = imports
+    result.update = update
+
+# ---------------------------------
+# hopefully can autogen above stuff
+
+proc update(ctx: Context, t: float) {.cdecl.} =
+    let pos = vec(120 + 10 * cos(5 * t), 200 + 100 * sin(t))
+    ctx.imports.drawBox(ctx.imports, pos, vec(40, 40))
+
+    ctx.imports.drawBox(ctx.imports, vec(300, 30), vec(40, 40))
